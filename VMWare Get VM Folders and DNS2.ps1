ae25a451dcb0e1ef52253a5d0ce162c366fbc549
HanSolo71 <HanSolo552@gmail.com>
2018-10-31 13:55:12 -0400

    Add files via upload

---
+++ VMWare Get VM Folders and DNS2.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module VMware.VimAutomation.Cis.Core
+Import-Module VMware.VimAutomation.Common
+Import-Module VMware.VimAutomation.Core
+
+
+Connect-VIServer vcenter.contoso.com
+get-vm | select folder,  name, @{N="DnsName"; E={$_.ExtensionData.Guest.Hostname}} | Export-CSV C:\Temp\DNStoVM.csv
